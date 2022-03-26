5c3b92217b3482a079d31dde752d8499fa957226
Yrrap <angus@Angus-Laptop.localdomain>
2022-03-26 19:56:26 +0000

    not sure

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -2,8 +2,8 @@
 
 
 int main(int argc, char* argv[]){
+	std::ofstream outfile(argv[4]);
 	yyin = fopen(argv[2], "r");
-	std::ofstream output_file(argv[4]);
 	Context context;
 	const Node* root = parse();
 	root->translate(context);
