a4f059117e386a926a46fbfd28f9908cb983ffc2
kert laansalu <kertlaansalu@gmail.com>
2022-03-29 17:40:52 +0100

    7 tests

--- out/bitwise_or.s
+++ out/bitwise_or.s
@@ -5,11 +5,13 @@ sw $fp, 4($sp)
 move $fp, $sp
 sw $a0, 12($fp)
 sw $a1, 16($fp)
-lw $2,12($fp)
-move $t0, $v0
-lw $2,16($fp)
-move $t1, $v0
-and $v0, $t0, $t1
+#placeholder for Declaration
+addiu $sp, $sp, -8
+addiu $sp, $sp,  8
+#placeholder for Declaration
+addiu $sp, $sp, -8
+addiu $sp, $sp,  8
+lw $2,0($fp)
 move $sp, $fp
 lw $fp, 4($sp)
 addiu $sp, $sp, 128
--- tmp/less_than.s
+++ tmp/less_than.s
@@ -5,6 +5,11 @@ sw $fp, 4($sp)
 move $fp, $sp
 sw $a0, 12($fp)
 sw $a1, 16($fp)
+lw $2,12($fp)
+move $t0, $v0
+lw $2,16($fp)
+move $t1, $v0
+slt $v0, $t0, $t1
 move $sp, $fp
 lw $fp, 4($sp)
 addiu $sp, $sp, 128
